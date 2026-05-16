54129d6e7c2ebdf7d4b7cc4072793d695134ad71
ElChurro274 <santinocolombo13@gmail.com>
2026-05-16 19:08:57 -0300

    Implemented everything to start the project

--- backend/setup.sh
+++ backend/setup.sh
@@ -4,6 +4,6 @@ echo "Configuring git hooks..."
 
 chmod +x .githooks/pre-push
 chmod +x .githooks/pre-commit
-git config core.hooksPath .githooks
+git config core.hooksPath backend/.githooks
 
 echo "Hooks configured successfully."
