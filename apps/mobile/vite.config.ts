b9a57f5961758dc9831fc31b65b2784c50c20a70
GarroteSantiago <sgarrote@mail.austral.edu.ar>
2026-05-19 16:37:32 -0300

    Fixed CI

--- apps/mobile/vite.config.ts
+++ apps/mobile/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   test: {
+    include: ['src/**/*.{test,spec}.{ts,tsx}'],
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
     passWithNoTests: true,
