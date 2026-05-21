7b83859bc3a676f697f81cbb00e4b462b2771775
ElChurro274 <santinocolombo13@gmail.com>
2026-05-21 14:02:56 -0300

    Finished the task

--- api/src/test/java/com/ssv/SsvApplicationTests.java
+++ api/src/test/java/com/ssv/SsvApplicationTests.java
@@ -4,15 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@SpringBootTest(properties = {
-		"edgar.api.key=test-edgar-key",
-		"yahoo.finance.api.key=test-yahoo-key",
-		"spring.security.oauth2.resourceserver.jwt.issuer-uri=https://test.auth0.com/",
-		"auth0.audience=https://api.test.com"
-})
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
 class SsvApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
 }
