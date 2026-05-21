3cd2f475004f5e07d87d99aea7745c1b47532a30
ElChurro274 <santinocolombo13@gmail.com>
2026-05-21 14:46:19 -0300

    test(edgar): add rate limiter coverage

--- api/src/test/java/com/ssv/edgar/infrastructure/ratelimit/MutableClock.java
+++ api/src/test/java/com/ssv/edgar/infrastructure/ratelimit/MutableClock.java
@@ -6,28 +6,28 @@ import java.time.ZoneId;
 
 public class MutableClock extends Clock {
 
-    private long millis;
+	private long millis;
 
-    MutableClock(long millis) {
-        this.millis = millis;
-    }
+	MutableClock(long millis) {
+		this.millis = millis;
+	}
 
-    void setMillis(long millis) {
-        this.millis = millis;
-    }
+	void setMillis(long millis) {
+		this.millis = millis;
+	}
 
-    @Override
-    public ZoneId getZone() {
-        return ZoneId.of("UTC");
-    }
+	@Override
+	public ZoneId getZone() {
+		return ZoneId.of("UTC");
+	}
 
-    @Override
-    public Clock withZone(ZoneId zone) {
-        return this;
-    }
+	@Override
+	public Clock withZone(ZoneId zone) {
+		return this;
+	}
 
-    @Override
-    public Instant instant() {
-        return Instant.ofEpochMilli(millis);
-    }
+	@Override
+	public Instant instant() {
+		return Instant.ofEpochMilli(millis);
+	}
 }
