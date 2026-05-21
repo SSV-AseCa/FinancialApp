c8e343e769d84fcfefbd270cfede9c706ad10236
ElChurro274 <santinocolombo13@gmail.com>
2026-05-21 14:06:21 -0300

    Finished the task

--- api/src/test/java/com/ssv/SsvApplicationTests.java
+++ api/src/test/java/com/ssv/SsvApplicationTests.java
@@ -5,11 +5,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.security.oauth2.resourceserver.jwt.issuer-uri=https://test.auth0.com/",
+		"auth0.audience=https://api.test.com"
+})
 class SsvApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
